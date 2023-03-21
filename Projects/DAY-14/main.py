f94b4ef9ccb51fd3a4083591455a88559e0f173e
Avinash <chunduriavinash6@gmail.com>
2023-03-21 18:11:59 +0530

    DAY-14 Followers Check

--- Projects/DAY-14/main.py
+++ Projects/DAY-14/main.py
@@ -3,7 +3,6 @@ import random
 from art import logo, vs
 from game_data import data
 
-score = 0
 
 def generate_description(data):
     return f"{data['name']}, a {data['description']}, from {data['country']}."
@@ -17,31 +16,31 @@ def get_data():
 
 
 def checking_condition(followers_check, followerCountOfA, followerCountOfB):
-    if (followers_check == 'A' and followerCountOfA > followerCountOfB) or \
-            (followers_check == 'B' and followerCountOfB > followerCountOfA):
-        global score
-        score += 1
-    elif followers_check in 'AB':
-        return True
+    if followerCountOfA > followerCountOfB:
+        return followers_check == 'A'
     else:
-        print("Wrong Choice")
-        return True
+        return followers_check == 'B'
 
 
 def game():
+    score = 0
     isGameOver = False
     print(logo)
     while not isGameOver:
         nameA, followerCountOfA = get_data()
-        if score != 0:
-            print(f"You're right! Current Score: {score}.")
+        nameB, followerCountOfB = get_data()
         print(f"Compare A: {nameA}")
         print(vs)
-        nameB, followerCountOfB = get_data()
         print(f"Aganist B: {nameB}")
         followers_check = input("Who has more more followers? Type 'A' or 'B':")
-        isGameOver = checking_condition(followers_check,followerCountOfA,followerCountOfB)
-    print(f"Sorry, that's wrong. Final Score: {score}")
+        isCheck = checking_condition(followers_check, followerCountOfA, followerCountOfB)
+
+        if isCheck:
+            score += 1
+            print(f"You're right! Current Score: {score}.")
+        else:
+            isGameOver = True
+            print(f"Sorry, that's wrong. Final Score: {score}")
 
 
 game()
