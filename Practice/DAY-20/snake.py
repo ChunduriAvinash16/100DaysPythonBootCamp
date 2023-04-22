143533b0b75a489588c4e54e6c30cd3d1b4ad7a1
Avinash <chunduriavinash6@gmail.com>
2023-04-22 23:57:17 +0530

    DAY-21 sanke 2

--- Practice/DAY-20/snake.py
+++ Practice/DAY-20/snake.py
@@ -7,6 +7,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -15,11 +16,19 @@ class Snake:
 
     def create_snake(self):
         for i in range(0, 3):
-            snake = Turtle(shape="square")
-            snake.color("white")
-            snake.penup()
-            snake.goto(STARTING_POSTION[i])
-            self.segments.append(snake)
+            self.add_segment(STARTING_POSTION[i])
+
+    def add_segment(self, position):
+        snake = Turtle(shape="square")
+        snake.color("white")
+        snake.penup()
+        snake.goto(position)
+        self.segments.append(snake)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+        print(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -43,5 +52,3 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
-
-
